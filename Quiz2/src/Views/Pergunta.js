2cca5f9f49a80a9ec97f6191e1a791800a272ede
JailsonPanizzon <jailson.panizzon@gmail.com>
2019-12-08 00:35:29 -0300

    ajustes

--- Quiz2/src/Views/Pergunta.js
+++ Quiz2/src/Views/Pergunta.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { ScreenOrientation } from "expo";
 import { RadioButton, Text } from "react-native-paper";
 
 const db = require("./pergunta.json");
 export default class Main extends Component {
   state = {
-    value: "first",
+    value: "",
     alternativas: []
   };
 
   componentDidMount() {
     let res = this.sortearAlterantivas();
-
     this.setState({ alternativas: res });
   }
 
@@ -21,17 +20,14 @@ export default class Main extends Component {
     let sorteadas = [];
     while (sorteadas.length < 4) {
       var aleatorio = Math.floor(Math.random() * 7);
-
       if (sorteadas.indexOf(aleatorio + 1) == -1) sorteadas.push(aleatorio + 1);
     }
-    console.log(listaAlterantivas);
     let alternativasSorteadas = [];
     aleatorio = Math.floor(Math.random() * 4);
     for (i = 0; i < 4; i++) {
       alternativasSorteadas.push(listaAlterantivas[sorteadas[i]]);
     }
     alternativasSorteadas[aleatorio] = listaAlterantivas[0];
-    console.log(alternativasSorteadas);
     return alternativasSorteadas;
   }
 
@@ -44,59 +40,98 @@ export default class Main extends Component {
   render() {
     this.changeScreenOrientation();
     return (
-      <View style={styles.container}>
-        <View style={styles.containerPergunta}>
-          <Text>{db.pergunta}</Text>
-        </View>
-        <View style={styles.containeralternativas}>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.containerPergunta}>
+            <Text>{db.pergunta}</Text>
+          </View>
           <RadioButton.Group
             onValueChange={value => this.setState({ value })}
             value={this.state.value}
+            style={styles.containeralternativas}
           >
             <View style={styles.alternativa}>
-              <Text>{this.state.alternativas[0]}</Text>
-              <RadioButton value="first" />
+              <RadioButton value={this.state.alternativas[0]} />
+              <Text
+                onPress={() => {
+                  this.setState({
+                    value: this.state.alternativas[0]
+                  });
+                }}
+              >
+                {this.state.alternativas[0]}
+              </Text>
             </View>
             <View style={styles.alternativa}>
-              <Text>{this.state.alternativas[1]}</Text>
-              <RadioButton value="second" />
+              <RadioButton value={this.state.alternativas[1]} />
+              <Text
+                onPress={() => {
+                  this.setState({
+                    value: this.state.alternativas[1]
+                  });
+                }}
+              >
+                {this.state.alternativas[1]}
+              </Text>
             </View>
             <View style={styles.alternativa}>
-              <Text>{this.state.alternativas[2]}</Text>
-              <RadioButton value="third" />
+              <RadioButton value={this.state.alternativas[2]} />
+              <Text
+                onPress={() => {
+                  this.setState({
+                    value: this.state.alternativas[2]
+                  });
+                }}
+              >
+                {this.state.alternativas[2]}
+              </Text>
             </View>
             <View style={styles.alternativa}>
-              <Text>{this.state.alternativas[3]}</Text>
-              <RadioButton value="fourty" />
+              <RadioButton value={this.state.alternativas[3]} />
+              <Text
+                onPress={() => {
+                  this.setState({
+                    value: this.state.alternativas[3]
+                  });
+                }}
+              >
+                {this.state.alternativas[3]}
+              </Text>
             </View>
           </RadioButton.Group>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
 const styles = StyleSheet.create({
   containerPergunta: {
-    width: "50%",
-    backgroundColor: "red",
-    flexDirection: "column",
+    width: "100%",
+    flexDirection: "row",
     alignItems: "stretch"
   },
+  radioButton: {
+    width: "10%",
+    alignItems: "flex-start",
+    flexDirection: "column"
+  },
+  label: {
+    width: "90%",
+    alignItems: "flex-start",
+    flexDirection: "column"
+  },
   containeralternativas: {
-    width: "100%",
-    backgroundColor: "blue",
     flexDirection: "column",
     alignItems: "stretch"
   },
   alternativa: {
-    width: "70%",
-    backgroundColor: "blue",
-    flexDirection: "column",
+    width: "100%",
+    flexDirection: "row",
     alignItems: "stretch"
   },
   container: {
     width: "100%",
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "stretch"
   }
 });
