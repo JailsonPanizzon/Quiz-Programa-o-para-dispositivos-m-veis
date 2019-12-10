148a90ec3b006afb6e6fb2571cca30971a84e878
Marlos Augusto <marlosaugusto@outlook.com>
2019-12-09 23:53:09 -0300

    style(question): change question style

---
+++ quizapp/src/assets/questions/index.js
@@ -0,0 +1,4 @@
+export const Geografia = require("./geografia.json")
+export const Historia = require("./historia.json")
+export const Informatica = require("./informatica.json")
+export const Matematica = require("./matematica.json")
