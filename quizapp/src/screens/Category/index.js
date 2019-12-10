4f3f5c3b555335b5b130610c7311e8fcf4e72082
JailsonPanizzon <jailson.panizzon@gmail.com>
2019-12-10 00:36:38 -0300

    ahhhhh

--- quizapp/src/screens/Category/index.js
+++ quizapp/src/screens/Category/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Text } from 'react-native-paper'
-import { ScrollView } from 'react-native'
+import React from 'react';
+import {Text} from 'react-native-paper';
+import {ScrollView} from 'react-native';
 
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
-import { Container } from '../../components/Container'
+import {Container} from '../../components/Container';
 
 const QuestionContainer = styled.View`
   width: 100%;
@@ -12,37 +12,72 @@ const QuestionContainer = styled.View`
   align-items: stretch;
 `;
 
-const db = require("../pergunta.json");
+const db = require('../pergunta.json');
 
-const Category = ({ navigation }) => {
-
+const Category = ({navigation}) => {
   const sortearPerguntas = () => {
-
-  }
+    let listaAlterantivas = db.alternativas;
+    let sorteadas = [];
+    while (sorteadas.length < 4) {
+      var aleatorio = Math.floor(Math.random() * 7);
+      if (sorteadas.indexOf(aleatorio + 1) == -1) sorteadas.push(aleatorio + 1);
+    }
+    let alternativasSorteadas = [];
+    aleatorio = Math.floor(Math.random() * 4);
+    for (i = 0; i < 4; i++) {
+      alternativasSorteadas.push(listaAlterantivas[sorteadas[i]]);
+    }
+    alternativasSorteadas[aleatorio] = listaAlterantivas[0];
+    return alternativasSorteadas;
+  };
 
   const handleBack = () => {
-    navigation.goBack()
-  }
+    navigation.goBack();
+  };
 
   return (
     <ScrollView>
-      <Container>
-        <QuestionContainer>
-          <Text>{db.pergunta}</Text>
-        </QuestionContainer>
-        <QuestionContainer>
-          <Text>{db.pergunta}</Text>
-        </QuestionContainer>
-        <QuestionContainer>
-          <Text>{db.pergunta}</Text>
-        </QuestionContainer>
-        <QuestionContainer>
-          <Text>{db.pergunta}</Text>
-        </QuestionContainer>
-      </Container>
-    </ScrollView>
+      <ScrollView>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.containerCategoria}>
+            <View style={styles.constainerButton}>
+              <Text>História</Text>
+            </View>
+            <Image
+              source={require("../img/historia.jpeg")}
+              style={styles.bg}
+            ></Image>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerCategoria}>
+            <View style={styles.constainerButton}>
+              <Text>Matemática</Text> 
+            </View>
+            <Image
+              source={require("../img/historia.jpeg")}
+              style={styles.bg}
+            ></Image>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerCategoria}>
+            <View style={styles.constainerButton}>
+              <Text>Informática</Text>
+            </View>
+            <Image
+              source={require("../img/historia.jpeg")}
+              style={styles.bg}
+            ></Image>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerCategoria}>
+            <View style={styles.constainerButton}>
+              <Text>Geografia</Text>
+            </View>
+            <Image
+              source={require("../img/historia.jpeg")}
+              style={styles.bg}
+            ></Image>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
   );
-}
+};
 
-
-export default Category
+export default Category;
